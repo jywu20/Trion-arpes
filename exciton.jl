f091a6e59ee015574d4b21cbba2737dd33e3df66
wujinq <wujinyuan111@126.com>
2023-10-22 14:16:12 -0400

    Finishing the exciton part

--- exciton.jl
+++ exciton.jl
@@ -21,7 +21,7 @@ end
 
 function arpes_colormap(gradient)
     rainbow = cgrad(:rainbow)
-    starting_region = gradient(first(rainbow), steps = 100) 
+    starting_region = gradient(first(rainbow), steps = 50) 
     middle_region = rainbow[LinRange(0, 1, 500)]
     end_region = reverse(gradient(last(rainbow), steps = 400))
     
@@ -33,40 +33,44 @@ function custom_colorbar(p, cb)
     plot(p, cb, layout = l)
 end
 
-
 # The exciton band index S and momentum Q should be pre-determined
-function single_exciton_arpes_signature(ϵ_v, Q, A_SQ, E_SQ, broadening, k_points, ω_points)
+function single_exciton_arpes_signature(ϵ_v, Q, A_SQ_k, E_SQ, broadening, k_points, ω_points)
     heatmap_points = Iterators.product(k_points, ω_points) 
 
     δ_factor = map(heatmap_points) do (k, ω)
-        broadening(ω - E_SQ - ϵ_v(k - Q))
+        broadening(ω - E_SQ(Q) - ϵ_v(k - Q))
     end
     
     exciton_structure_factor = map(heatmap_points) do (k, ω)
-        abs(A_SQ(k))^2
+        abs(A_SQ_k(Q, k))^2
     end
     
     A_kω = exciton_structure_factor .* δ_factor
     A_kω
 end
 
-# Example of ARPES plot 
+function Q_distribution(E_SQ, β, Q_points)
+    unnormalized_distribution = map(Q_points) do Q
+        exp(- β * E_SQ(Q))
+    end
+    unnormalized_distribution / sum(unnormalized_distribution)
+end
 
 const fs = 1.5193
-eV = 3.80998
+const eV = 3.80998
 
 p = let σ = 20fs, # Note that here σ tells us the width of the pulse; it should be *large* to produce δ-function like ARPES spectrum
     m_v = 0.8184,
     m_c = 0.4794,
     E_g = 0.7830,
     ϵ = 6.4, 
-    E_B = 0.1,
+    E_B = -0.1,
     w = 0.7,
-    Q = w + 0.2,
     β = 100,
     broadening(x) = exp(- σ^2 * x^2),
     k_points = LinRange(-0.5, 1.2, 500),
     ω_points = LinRange(-0.4, 1.2, 500),
+    Q_points = k_points
 
     ϵ_v(k) = - norm(k)^2 / 2m_v * eV
     ϵ_c(k) = E_g + norm(k - w)^2 / 2m_c * eV
@@ -75,21 +79,37 @@ p = let σ = 20fs, # Note that here σ tells us the width of the pulse; it shoul
     α = m_c / M
     μ = m_c * m_v / (m_c + m_v)
     a_ex = 0.529 * ϵ / μ
-    A_SQ(k) = 1 / (1 + norm(k - (w + α * (Q - w)))^2 * a_ex^2 / 4)^2
-    E_SQ = E_g - E_B
+    A_SQ(Q, k) = 1 / (1 + norm(k - (w + α * (Q - w)))^2 * a_ex^2 / 4)^2
+    E_SQ(Q) = E_g + E_B + (Q - w)^2/2M * eV
 
-
-    A_kω = single_exciton_arpes_signature(ϵ_v, Q, A_SQ, E_SQ, broadening, k_points, ω_points)
+    A_kω_Q = map(Q_points) do Q
+        single_exciton_arpes_signature(ϵ_v, Q, A_SQ, E_SQ, 
+            broadening, k_points, ω_points)
+    end
+    f_Q = Q_distribution(E_SQ, β, Q_points)
+    A_kω = sum(f_Q .* A_kω_Q) 
     
     p = heatmap(k_points, ω_points, A_kω', 
         c = arpes_colormap(transparency_gradience),
         colorbar = false) 
     plot!(p, k_points, ϵ_v.(k_points), 
         legend = false, 
-        linestyle = :dash)
+        linestyle = :dash,
+        c = colorant"lightskyblue3")
     plot!(p, k_points, ϵ_c.(k_points), 
         legend = false, 
-        linestyle = :dash)
+        linestyle = :dash,
+        c = colorant"lightskyblue3")
+
+    k_points_near_valley = LinRange(0.4, 1.0, 100)
+    plot!(p, k_points_near_valley, ϵ_c.(k_points_near_valley) .+ E_B,
+        legend = false,
+        linestyle = :dot,
+        c = colorant"aqua")
+    plot!(p, k_points_near_valley, ϵ_v.(k_points_near_valley .- w) .+ (E_g + E_B),
+        legend = false,
+        linestyle = :dot,
+        c = colorant"aqua")
     
     xlims!(p, (-0.5, 1.2))
     ylims!(p, (-0.4, 1.2)) 
